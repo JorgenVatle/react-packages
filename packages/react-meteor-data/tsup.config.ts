d98806e7d899381ce25654356e6270c80ec371a8
Jørgen Vatle <Jorgen@WcAServices.net>
2024-02-20 19:32:21 +0100

    Externalize meteor packages

--- packages/react-meteor-data/tsup.config.ts
+++ packages/react-meteor-data/tsup.config.ts
@@ -3,5 +3,6 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
     entry: ['index.js'],
     outDir: 'dist',
-    format: ['esm', 'cjs']
+    format: ['esm', 'cjs'],
+    external: ['meteor/meteor', 'meteor/mongo', 'meteor/tracker']
 })
